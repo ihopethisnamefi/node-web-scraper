dbbdcf17fc34df4f5237b51a201494f8bea6df8e
ihopethisnamefi <jonthibeault@gmail.com>
2017-10-18 20:38:51 -0500

    League Data Scraping Working

--- server.js
+++ server.js
@@ -1,21 +1,38 @@
 let axios = require('axios');
 let cheerio = require('cheerio');
 
-let base_url = 'http://games.espn.com/ffl/standings?leagueId=164548&seasonId=2017';
+let base_url = 'http://games.espn.com/ffl/standings?leagueId=164569&seasonId=2017';
 
 axios.get(base_url).then( (response) => {
   let $ = cheerio.load(response.data);
   let users = [];
+  let pointsArray = [];
+  $('.evenRow, .oddRow').each( (i, elm) => {
+      pointsArray.push({
+          pointsFor: $(elm).children().eq(1).attr('class','sortablePF').text(),
+          pointsAgainst: $(elm).children().eq(2).attr('class','sortablePA').text(),
+      });
+  });
+
   $('.tableBody [bgcolor=#f2f2e8], .tableBody [bgcolor=#f8f8f2]').each( (i, elm) => {
     users.push( {
       team: $(elm).children().first().children().attr('title'),
       wins: $(elm).children().eq(1).first().text(),
-      losses: $(elm).children().eq(2).first().text(),
+      losses: $(elm).children().eq(2).first().text(),  
     });
   });
 
+  //console.log(pointsArray);
+  for(i in users) {
+    for(j in pointsArray){
+      if (i == j){
+        users[i].pf = pointsArray[j].pointsFor;
+        users[i].pa = pointsArray[j].pointsAgainst;
+      }
+    }
+  }
   return(users);
 })
 .then ( (users) => {
-  console.log(users);
+ console.log(users);
 });
